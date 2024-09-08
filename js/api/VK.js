3d8f9298e54d3cc95895ef609e30bb5d06c15fe4
backsty <shalamovartem6@mail.ru>
2024-09-08 21:34:04 +0700

    Made an api for getting photos from a VK profile

--- js/api/VK.js
+++ js/api/VK.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * Класс VK
  * Управляет изображениями из VK. С помощью VK API.
@@ -6,21 +8,47 @@
  * */
 class VK {
 
-  static ACCESS_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008';
-  static lastCallback;
+  static ACCESS_TOKEN = window.env.ACCESS_TOKEN;
+  static lastCallback = () => {};
 
   /**
-   * Получает изображения
-   * */
+   * Получает изображения из профиля VK.
+   * @param {string} id - ID пользователя VK.
+   * @param {function} callback - Функция обратного вызова для обработки полученных данных.
+   */
   static get(id = '', callback){
+    this.lastCallback = callback;
 
+    let script = document.createElement('SCRIPT');
+    script.src = `https://api.vk.com/method/photos.get?owner_id=${id}&album_id=profile&photo_sizes=1&count=1000&access_token=${this.ACCESS_TOKEN}&v=5.199&callbakc=VK.lastCallback.callbackfn`;
+    document.getElementsByTagName('head')[0].appendChild(script);
   }
 
   /**
    * Передаётся в запрос VK API для обработки ответа.
    * Является обработчиком ответа от сервера.
+   * * @param {Object} result - Ответ от VK API.
    */
   static processData(result){
+    document.head.querySelector('script[src*="photos.get"]').remove();
 
+    if (!result || result.error) {
+      alert(result.error?.error_msg ?? 'Ошибка при загрузке изображений');
+      return;
+    }
+
+    if (result.response.items.lenght === 0) {
+      alert('Изображения не найдены в профиле.');
+      return;
+    }
+
+    const images = result.response.items.map(item => {
+      return item.sizes.reduce((largest, size) => {
+        return size.width * size.height > largest.width * largest.height ? size : largest;
+      }, item.sizes[0]);
+    });
+
+    this.lastCallback(images);
+    this.lastCallback = () => {};
   }
 }
